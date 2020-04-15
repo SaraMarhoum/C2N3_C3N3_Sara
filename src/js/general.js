d5ae205c6407ae000723bac4deae4e5d8c10539a
SaraMarhoum <57989282+SaraMarhoum@users.noreply.github.com>
2020-04-15 14:25:12 +0100

    correct css Q1

--- src/js/general.js
+++ src/js/general.js
@@ -9,7 +9,15 @@
 function displayQu(){
    const pré = document.getElementById("Préambule");
    const qu1 = document.getElementById("Q1");
+   const line = document.getElementById("time_line");
+   const position1 = document.getElementById("test_position");
+   const position2 = document.getElementById("test_position_questions");
+   
 
    pré.style.display = "none";
    qu1.style.display = "block";
-};
+   line.style.display = "block";
+   position1.style.display = "none";
+   position2.style.display = "block";
+   
+}
