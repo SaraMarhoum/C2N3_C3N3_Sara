13e2fa2e112f1b4e0c3073b087ef8ceb40a23669
SaraMarhoum <57989282+SaraMarhoum@users.noreply.github.com>
2020-04-15 12:36:20 +0100

    Add JS to display test

---
+++ src/js/general.js
@@ -0,0 +1,15 @@
+
+// display test start when click button demarrer le test
+
+// function startTest(){
+    
+// }
+
+// switch from préambule to 1st question
+function displayQu(){
+   const pré = document.getElementById("Préambule");
+   const qu1 = document.getElementById("Q1");
+
+   pré.style.display = "none";
+   qu1.style.display = "block";
+};
