26bb70ec22135e0b47a77670abd528ad2bc6b450
SaraMarhoum <57989282+SaraMarhoum@users.noreply.github.com>
2020-04-18 13:28:02 +0100

    organize code

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,13 @@
-   
-   
-   
-
 
 // display test start when click button demarrer le test
    // switch from préambule to 1st question
    
 function displayQu(){
-   const pré = document.getElementById("Préambule");
-   const qu1 = document.getElementById("Q1");
-   const line = document.getElementById("time_line");
-   const position1 = document.getElementById("test_position");
-   const position2 = document.getElementById("test_position_questions");
+   var pré = document.getElementById("Préambule");
+   var qu1 = document.getElementById("Q1");
+   var line = document.getElementById("time_line");
+   var position1 = document.getElementById("test_position");
+   var position2 = document.getElementById("test_position_questions");
    pré.style.display = "none";
    qu1.style.display = "block";
    line.style.display = "block";
@@ -21,54 +17,58 @@ function displayQu(){
 
   //display 1st stepper question after cheking age
 function startQ(){
-var qStepper  = document.getElementById("questionsStepper");
-const qu1 = document.getElementById("Q1");
-var showN = document.getElementById("showNext");
-var showP = document.getElementById("showPrev");
-   qu1.style.display = "none";
-   qStepper.style.display = "block";
-   showN.style.display = "block";
-   showP.style.display = "block";
-
-
+  var qStepper  = document.getElementById("questionsStepper");
+  const qu1 = document.getElementById("Q1");
+  var showN = document.getElementById("showNext");
+  var showP = document.getElementById("showPrev");
+   
+  qu1.style.display = "none";
+  qStepper.style.display = "block";
+  showN.style.display = "block";
+  showP.style.display = "block";
 }
 
-   // display next questions every click on question suivante
-   
-var visibleQ = 0;
+  // display next questions every click on question suivante using Jquery
+  var visibleQ = 0;
 
-   function showQ(){
+  function showQ(){
      
-     $(".Q").hide();
-     $(".Q:eq("+ visibleQ +")").show();
-   }
-   showQ()
+    $(".Q").hide();
+    $(".Q:eq("+ visibleQ +")").show();
+  }
+   
+  showQ()
 
-   function showNext(){
-     if(visibleQ < 20){
-       if(visibleQ== $(".Q").length-1){
-       visibleQ = 0;
-     }
-     else{
-       visibleQ ++;
-     }
+  function showNext(){
+    if(visibleQ < 20){
+      if(visibleQ== $(".Q").length-1){
+        visibleQ = 0;
+      }
+      else{
+        visibleQ ++;
+      }
      showQ();
-     }
-   }
+    }
+  }
 
-   // display peevious questions every click on question précédente
-   function showPrev(){
+  // display peevious questions every click on question précédente using Jquery
+  function showPrev(){
     if(visibleQ > 0){
       if(visibleQ == 0){
         visibleQ = $(".Q").length-1;
-      }
+        }
       else{
         visibleQ --;
-      }
+        }
       showQ();
+    } 
+  }
 
-    }
-      
-    }
+//make time line incremant with question ++
 
-    
+//decide if continue afer age question 
+  // (if input < 15 ==> alert && get back to accueil)
+  // (if input >= 15 ==> continue question stepper)
+
+//Collect answers and give result depanding on inputs
+
