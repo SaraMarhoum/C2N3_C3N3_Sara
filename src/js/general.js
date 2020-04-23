b8f25ad88fa2869024a1e95864d4b70f25be4b5e
SaraMarhoum <57989282+SaraMarhoum@users.noreply.github.com>
2020-04-23 12:16:33 +0000

    adding age condition

--- src/js/general.js
+++ src/js/general.js
@@ -13,17 +13,41 @@ function displayQu(){
    position2.style.display = "block";
 }
 
+//decide if continue afer age question 
+  // (if input < 15 ==> alert && get back to accueil)
+  // (if input >= 15 ==> continue question stepper)
+
+  function ageCondition(){
+    var pré = document.getElementById("Préambule");
+    var qu1 = document.getElementById("Q1");
+    var position1 = document.getElementById("test_position");
+    var position2 = document.getElementById("test_position_questions");
+
+      alert("Prenez contact avec votre médecin généraliste au moindre doute. Cette application n’est pour l’instant pas adaptée aux personnes de moins de 15 ans. En cas d’urgence, appeler le 15.")
+      pré.style.display = "block";
+      qu1.style.display = "none";
+      position1.style.display = "block";
+      position2.style.display = "none";   
+  }
+
   //display 1st stepper question after cheking age
 function startQ(){
+  let AgeInput = document.getElementById("ageInp").value;
+
   var qStepper  = document.getElementById("questionsStepper");
   const qu1 = document.getElementById("Q1");
   var showN = document.getElementById("showNext");
   var showP = document.getElementById("showPrev");
-   
-  qu1.style.display = "none";
+
+  if(AgeInput < 15){
+    ageCondition();
+  }
+   else{
+     qu1.style.display = "none";
   qStepper.style.display = "block";
   showN.style.display = "block";
   showP.style.display = "block";
+   }
 }
 
   // display next questions every click on question suivante using Jquery
@@ -79,13 +103,8 @@ function displaycount(){
     } 
   }
 
-//make time line incremant with question ++ using JS
 
-
-
-//decide if continue afer age question 
-  // (if input < 15 ==> alert && get back to accueil)
-  // (if input >= 15 ==> continue question stepper)
 
 //Collect answers and give result depanding on inputs
 
+//make time line incremant with question ++ using JS
